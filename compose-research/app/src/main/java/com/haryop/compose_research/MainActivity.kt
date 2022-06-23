cd78ded528613297362023429850043a14d1af7e
bayoharyo <haryoparigroho@gmail.com>
2022-06-23 19:46:25 +0700

    add lazy column state

--- compose-research/app/src/main/java/com/haryop/compose_research/MainActivity.kt
+++ compose-research/app/src/main/java/com/haryop/compose_research/MainActivity.kt
@@ -6,12 +6,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -38,10 +38,21 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     private fun Conversation(messages: List<Message>) {
-        LazyColumn {
+        val state = rememberLazyListState()
+        LazyColumn(
+            state = state
+        ) {
             items(messages) { message ->
                 MessageCard(message)
             }
+        }
+        val showScrollToTopButton by remember {
+            derivedStateOf {
+                state.firstVisibleItemIndex > 0
+            }
+        }
+        if (showScrollToTopButton) {
+            // Show scroll to top button here
         }
     }
 
